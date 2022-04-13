243225e78b8debc3d8d1289411cf4ff714133675
ysuprism <yasuhiro0317@keio.jp>
2022-04-13 17:24:25 +0000

    fade in contents

--- trinity/pages/index.tsx
+++ trinity/pages/index.tsx
@@ -2,31 +2,51 @@ import type { NextPage } from 'next';
 import Head from 'next/head';
 import Image from 'next/image';
 import Link from 'next/link';
+import React, { RefObject, useEffect, useRef } from 'react';
 import Layout from '../components/layout';
 
 const Home: NextPage = () => {
+  const inputRef = useRef<HTMLDivElement>(null);
+  const inputRef2 = useRef<HTMLDivElement>(null);
+  const inputRef3 = useRef<HTMLDivElement>(null);
+  const inputRef4 = useRef<HTMLDivElement>(null);
+  useEffect(() => {
+    const handle = (ref: RefObject<HTMLDivElement>) => () => {
+      if (!ref.current) return;
+      if (ref.current.offsetTop < window.scrollY + window.innerHeight - 150) {
+        ref.current.style.opacity = '1';
+        ref.current.style.transition = '2s';
+      }
+    };
+    window.addEventListener('scroll', handle(inputRef));
+    window.addEventListener('scroll', handle(inputRef2));
+    window.addEventListener('scroll', handle(inputRef3));
+    window.addEventListener('scroll', handle(inputRef4));
+  }, []);
   return (
     <Layout>
       <Head>
         <title>トップページ</title>
       </Head>
-      <Image src='/images/eth.png' width={400} height={300} layout='responsive'></Image>
+      <Image src='/images/eth.png' width={400} height={300} layout='responsive' alt='ETH'></Image>
       <section id='title'>
         <div className='center'>
-          <p>家庭教師　個人契約の新たな形態</p>
+          <div>
+            <p>家庭教師　個人契約の新たな形態</p>
+          </div>
         </div>
       </section>
       <section id='content'>
-        <div className='left'>
+        <div className='left' ref={inputRef}>
           <p>チームでサポート</p>
         </div>
-        <div className='right'>
+        <div className='right' ref={inputRef2}>
           <p>コンサルタントが最適な教師を紹介</p>
         </div>
-        <div className='left'>
+        <div className='left' ref={inputRef3}>
           <p>選び抜かれた参考書</p>
         </div>
-        <div className='right'>
+        <div className='right' ref={inputRef4}>
           <p>無理のないカリキュラムの作成</p>
         </div>
       </section>
@@ -37,7 +57,7 @@ const Home: NextPage = () => {
       </section>
       <section id='process'>
         <div className='center'>
-          <p>契約までの流れ</p>
+          <p>ご契約までの流れ</p>
         </div>
       </section>
       <style jsx>{`
@@ -47,7 +67,11 @@ const Home: NextPage = () => {
           border: 3px solid black;
           border-radius: 40px;
           background-color: #eeeeee;
-          text-align: center;
+          display: flex;
+          justify-content: center;
+          align-items: center;
+          margin-top: 50px;
+          margin-bottom: 50px;
         }
         section#title p {
           width: 700px;
@@ -72,6 +96,9 @@ const Home: NextPage = () => {
         }
         section#content {
           background-color: wheat;
+        }
+        section#content div {
+          opacity: 0;
         }
         section#price {
           background-color: #000099;
