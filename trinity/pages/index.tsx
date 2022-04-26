9db80f4c07c0e72a4aeeb6b6c310c1cf75e90ab3
ysuprism <yasuhiro0317@keio.jp>
2022-04-26 08:18:42 +0000

    trigger slide-in animation on scroll

--- trinity/pages/index.tsx
+++ trinity/pages/index.tsx
@@ -8,6 +8,129 @@ import React, { RefObject, useEffect, useRef, useState } from 'react';
 import Layout from '../components/layout';
 
 const Home: NextPage = () => {
+  const [triggered1, setTriggered1] = useState(false);
+  const [triggered2, setTriggered2] = useState(false);
+  const [triggered3, setTriggered3] = useState(false);
+  const [triggered4, setTriggered4] = useState(false);
+  const [triggered5, setTriggered5] = useState(false);
+  const [triggered6, setTriggered6] = useState(false);
+  const [triggered7, setTriggered7] = useState(false);
+  const ref1 = useRef<HTMLDivElement | null>(null);
+  const ref2 = useRef<HTMLDivElement | null>(null);
+  const ref3 = useRef<HTMLDivElement | null>(null);
+  const ref4 = useRef<HTMLDivElement | null>(null);
+  const ref5 = useRef<HTMLDivElement | null>(null);
+  const ref6 = useRef<HTMLDivElement | null>(null);
+  const ref7 = useRef<HTMLDivElement | null>(null);
+
+  const scrollHandler1 = () => {
+    if (ref1 && ref1.current) {
+      const rect = ref1.current.getBoundingClientRect();
+      const top = rect.top;
+      if (top < window.innerHeight / 2) {
+        ref1.current.classList.add('ani3');
+        setTriggered1(true);
+      }
+    }
+  };
+
+  const scrollHandler2 = () => {
+    if (ref2 && ref2.current) {
+      const rect = ref2.current.getBoundingClientRect();
+      const top = rect.top;
+      if (top < window.innerHeight / 2) {
+        ref2.current.classList.add('ani-left');
+        setTriggered2(true);
+      }
+    }
+  };
+
+  const scrollHandler3 = () => {
+    if (ref3 && ref3.current) {
+      const rect = ref3.current.getBoundingClientRect();
+      const top = rect.top;
+      if (top < window.innerHeight / 2) {
+        ref3.current.classList.add('ani-right');
+        setTriggered3(true);
+      }
+    }
+  };
+
+  const scrollHandler4 = () => {
+    if (ref4 && ref4.current) {
+      const rect = ref4.current.getBoundingClientRect();
+      const top = rect.top;
+      if (top < window.innerHeight / 2) {
+        ref4.current.classList.add('ani-left');
+        setTriggered4(true);
+      }
+    }
+  };
+
+  const scrollHandler5 = () => {
+    if (ref5 && ref5.current) {
+      const rect = ref5.current.getBoundingClientRect();
+      const top = rect.top;
+      if (top < window.innerHeight / 2) {
+        ref5.current.classList.add('ani-right');
+        setTriggered5(true);
+      }
+    }
+  };
+
+  const scrollHandler6 = () => {
+    if (ref6 && ref6.current) {
+      const rect = ref6.current.getBoundingClientRect();
+      const top = rect.top;
+      if (top < window.innerHeight / 2) {
+        ref6.current.classList.add('ani3');
+        setTriggered6(true);
+      }
+    }
+  };
+
+  const scrollHandler7 = () => {
+    if (ref7 && ref7.current) {
+      const rect = ref7.current.getBoundingClientRect();
+      const top = rect.top;
+      if (top < (window.innerHeight * 2) / 3) {
+        ref7.current.classList.add('ani3');
+        setTriggered7(true);
+      }
+    }
+  };
+
+  useEffect(() => {
+    if (!triggered1) {
+      window.addEventListener('scroll', scrollHandler1);
+      return () => window.removeEventListener('scroll', scrollHandler1);
+    }
+    if (!triggered2) {
+      window.addEventListener('scroll', scrollHandler2);
+      return () => window.removeEventListener('scroll', scrollHandler2);
+    }
+    if (!triggered3) {
+      window.addEventListener('scroll', scrollHandler3);
+      return () => window.removeEventListener('scroll', scrollHandler3);
+    }
+    if (!triggered4) {
+      window.addEventListener('scroll', scrollHandler4);
+      return () => window.removeEventListener('scroll', scrollHandler4);
+    }
+    if (!triggered5) {
+      window.addEventListener('scroll', scrollHandler5);
+      return () => window.removeEventListener('scroll', scrollHandler5);
+    }
+    if (!triggered6) {
+      window.addEventListener('scroll', scrollHandler6);
+      return () => window.removeEventListener('scroll', scrollHandler6);
+    }
+    if (!triggered7) {
+      window.addEventListener('scroll', scrollHandler7);
+      return () => window.removeEventListener('scroll', scrollHandler7);
+    }
+  }, [triggered1, triggered2, triggered3, triggered4, triggered5, triggered6, triggered7]);
+
   return (
     <Layout>
       <Head>
@@ -23,16 +146,7 @@ const Home: NextPage = () => {
         <div className={'triangle3-wrap'}>
           <div className={'triangle3'}></div>
         </div>
-        <div
-          style={{
-            width: '50%',
-            height: '100%',
-            position: 'absolute',
-            background: 'yellow',
-            zIndex: '1',
-            animation: 'animation1 2s ease-in 0s forwards',
-          }}
-        ></div>
+        <div className={'bg'}></div>
         <Typography
           fontWeight={'bold'}
           fontSize={200}
@@ -49,29 +163,37 @@ const Home: NextPage = () => {
         </Typography>
       </div>
       <section id={'title'}>
-        <div>家庭教師　個人契約の新たな形態</div>
+        <div ref={ref1}>家庭教師　個人契約の新たな形態</div>
       </section>
       <section id={'content'}>
         <ul style={{ listStyleType: 'none' }}>
           <li>
-            <div className={'item1'}>チームでサポート</div>
+            <div className={'item1'} ref={ref2}>
+              チームでサポート
+            </div>
           </li>
           <li>
-            <div className={'item2'}>コンサルタントが最適な教師を紹介</div>
+            <div className={'item2'} ref={ref3}>
+              コンサルタントが最適な教師を紹介
+            </div>
           </li>
           <li>
-            <div className={'item3'}>選び抜かれた参考書</div>
+            <div className={'item3'} ref={ref4}>
+              選び抜かれた参考書
+            </div>
           </li>
           <li>
-            <div className={'item4'}>無理のないカリキュラムの作成</div>
+            <div className={'item4'} ref={ref5}>
+              無理のないカリキュラムの作成
+            </div>
           </li>
         </ul>
       </section>
       <section id={'price'}>
-        <div>料金</div>
+        <div ref={ref6}>料金</div>
       </section>
       <section id={'process'}>
-        <div>ご契約までの流れ</div>
+        <div ref={ref7}>ご契約までの流れ</div>
       </section>
       <style jsx>{`
         ul {
@@ -94,7 +216,14 @@ const Home: NextPage = () => {
           text-align: center;
           border: 40px solid black;
           border-radius: 40px;
-          animation: animation3 2s ease-in 5s forwards;
+        }
+        .bg {
+          width: 50%;
+          height: 100%;
+          position: absolute;
+          background: yellow;
+          zindex: 1;
+          animation: animation1 2s ease-in 0s forwards;
         }
         #content {
           width: 100%;
@@ -115,23 +244,19 @@ const Home: NextPage = () => {
           top: 2.5%;
           left: 0%;
           transform: translateX(-100%);
-          animation: animation-left 2s ease-in 6.5s forwards;
         }
         .item2 {
           top: 27.5%;
           left: 100%;
-          animation: animation-right 2s ease-in 8.5s forwards;
         }
         .item3 {
           top: 52.5%;
           left: 0%;
           transform: translateX(-100%);
-          animation: animation-left 2s ease-in 10.5s forwards;
         }
         .item4 {
           top: 77.5%;
           left: 100%;
-          animation: animation-right 2s ease-in 12.5s forwards;
         }
         #price {
           width: 100%;
@@ -150,7 +275,6 @@ const Home: NextPage = () => {
           text-align: center;
           border: 40px solid black;
           border-radius: 40px;
-          animation: animation3 2s ease-in 13s forwards;
         }
         #process {
           width: 100%;
@@ -169,7 +293,6 @@ const Home: NextPage = () => {
           text-align: center;
           border: 40px solid black;
           border-radius: 40px;
-          animation: animation3 2s ease-in 15s forwards;
         }
         .triangle1-wrap {
           width: 200px;
@@ -257,15 +380,24 @@ const Home: NextPage = () => {
             transform: translate(12.5%, -50%);
           }
         }
+        .ani3 {
+          animation: animation3 1s ease-in 0s forwards;
+        }
         @keyframes animation-left {
           100% {
             transform: translateX(12.5%);
           }
         }
+        .ani-left {
+          animation: animation-left 0.5s ease-in 0s forwards;
+        }
         @keyframes animation-right {
           100% {
             transform: translateX(-112.5%);
           }
+        }
+        .ani-right {
+          animation: animation-right 0.5s ease-in 0s forwards;
         }
       `}</style>
     </Layout>
