c88216d52091225169cdd5fb9c81bd1d19ac0693
ysuprism <yasuhiro0317@keio.jp>
2022-05-15 20:11:38 +0000

    stripe border design and eliminate redundant code

--- trinity/pages/index.tsx
+++ trinity/pages/index.tsx
@@ -1,4 +1,3 @@
-import { createContext } from 'vm';
 import { Typography } from '@mui/material';
 import type { NextPage } from 'next';
 import Head from 'next/head';
@@ -51,11 +50,11 @@ const Home: NextPage = () => {
         const top = rect.top;
         if (ref === divRef7) {
           if (top < (window.innerHeight * 2) / 3) {
-            ref.current.classList.add('ani2');
+            ref.current.classList.add('fade-in');
           }
         } else {
           if (top < window.innerHeight / 2) {
-            ref.current.classList.add('ani2');
+            ref.current.classList.add('fade-in');
           }
         }
       }
@@ -91,29 +90,29 @@ const Home: NextPage = () => {
   useEffect(() => {
     const canvas = canvasRef.current;
     const canvas2 = canvasRef2.current;
-    const displayWidth = canvas.clientWidth;
-    const displayWidth2 = canvas2.clientWidth;
-    const displayHeight = canvas.clientHeight;
-    const displayHeight2 = canvas2.clientHeight;
+    const displayWidth = canvas?.clientWidth;
+    const displayWidth2 = canvas2?.clientWidth;
+    const displayHeight = canvas?.clientHeight;
+    const displayHeight2 = canvas2?.clientHeight;
 
-    if (canvas.width != displayWidth || canvas.height != displayHeight) {
-      canvas.width = displayWidth;
-      canvas.height = displayHeight;
+    if (canvas?.width != displayWidth || canvas?.height != displayHeight) {
+      canvas?.width = displayWidth;
+      canvas?.height = displayHeight;
     }
 
-    if (canvas2.width != displayWidth2 || canvas2.height != displayHeight2) {
-      canvas2.width = displayWidth;
-      canvas2.height = displayHeight;
+    if (canvas2?.width != displayWidth2 || canvas2?.height != displayHeight2) {
+      canvas2?.width = displayWidth;
+      canvas2?.height = displayHeight;
     }
 
     const img = new Image();
     img.src = 'images/カラフル１.jpg';
     img.onload = () => {
       const canvas = canvasRef2.current;
-      const ctx = canvas.getContext('2d');
-      ctx?.clearRect(0, 0, ctx.width, ctx.height);
+      const ctx = canvas?.getContext('2d');
+      ctx?.clearRect(0, 0, canvas?.width, canvas?.height);
       const pattern = ctx?.createPattern(img, 'no-repeat');
-      ctx.fillStyle = pattern;
+      ctx?.fillStyle = pattern;
       ctx?.beginPath();
       ctx?.moveTo(85, 50);
       ctx?.lineTo(85, 550);
@@ -147,8 +146,8 @@ const Home: NextPage = () => {
   useEffect(() => {
     const canvas = canvasRef.current;
     for (let i = 0; i < num; i++) {
-      const coordX = Math.random() * canvas.width;
-      const coordY = Math.floor(Math.random() * canvas.height);
+      const coordX = Math.random() * canvas?.width;
+      const coordY = Math.floor(Math.random() * canvas?.height);
       const radius = Math.floor(Math.random() * 20);
       circle.push({ x: coordX, y: coordY, r: radius });
     }
@@ -160,13 +159,13 @@ const Home: NextPage = () => {
 
   useEffect(() => {
     const canvas = canvasRef.current;
-    const ctx = canvas.getContext('2d');
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    ctx.fillStyle = '#EEEEEE';
+    const ctx = canvas?.getContext('2d');
+    ctx?.clearRect(0, 0, canvas?.width, canvas?.height);
+    ctx?.fillStyle = '#EEEEEE';
     for (let i = 0; i < num; i++) {
-      ctx.beginPath();
-      ctx.arc(circle[i].x, circle[i].y, circle[i].r, 0, Math.PI * 2, true);
-      ctx.fill();
+      ctx?.beginPath();
+      ctx?.arc(circle[i].x, circle[i].y, circle[i].r, 0, Math.PI * 2, true);
+      ctx?.fill();
     }
   }, [circle]);
 
@@ -177,29 +176,23 @@ const Home: NextPage = () => {
       </Head>
       <div style={{ width: '100%', height: '700px', position: 'relative' }}>
         <canvas
+          className='center'
           width={700}
           height={700}
           ref={canvasRef}
           style={{
             width: '100%',
             height: '700px',
-            position: 'absolute',
-            top: '50%',
-            left: '50%',
-            transform: 'translate(-50%, -50%)',
           }}
         ></canvas>
         <canvas
+          className='center'
           width={600}
           height={600}
           ref={canvasRef2}
           style={{
             width: '600px',
             height: '600px',
-            position: 'absolute',
-            top: '50%',
-            left: '50%',
-            transform: 'translate(-50%, -50%)',
           }}
         ></canvas>
         <Typography
@@ -211,155 +204,144 @@ const Home: NextPage = () => {
             top: '50%',
             left: '50%',
             transform: 'translate(-50%, -50%)',
-            zIndex: '5',
           }}
         >
           Medical Trinity
         </Typography>
       </div>
-      <section id={'title'}>
-        <div ref={divRef}>家庭教師　個人契約の新たな形態</div>
+      <section id='title'>
+        <div className='outer center' ref={divRef}>
+          <div className='inner center'>
+            <h1>家庭教師　個人契約の新たな形態</h1>
+            <p>coming soon</p>
+          </div>
+        </div>
       </section>
-      <section id={'content'}>
+      <section id='content'>
         <ul style={{ listStyleType: 'none' }}>
           <li>
-            <div className={'item1'} ref={divRef2}>
-              チームでサポート
+            <div className='outer item1' ref={divRef2}>
+              <div className='inner center'>
+                <h1>チームでサポート</h1>
+                <p>coming soon</p>
+              </div>
             </div>
           </li>
           <li>
-            <div className={'item2'} ref={divRef3}>
-              コンサルタントが最適な教師を紹介
+            <div className='outer item2' ref={divRef3}>
+              <div className='inner center'>
+                <h1>コンサルタントが最適な教師を紹介</h1>
+                <p>coming soon</p>
+              </div>
             </div>
           </li>
           <li>
-            <div className={'item3'} ref={divRef4}>
-              選び抜かれた参考書
+            <div className='outer item3' ref={divRef4}>
+              <div className='inner center'>
+                <h1>厳選された参考書</h1>
+                <p>coming soon</p>
+              </div>
             </div>
           </li>
           <li>
-            <div className={'item4'} ref={divRef5}>
-              無理のないカリキュラムの作成
+            <div className='outer item4' ref={divRef5}>
+              <div className='inner center'>
+                <h1>無理のないカリキュラムの作成</h1>
+                <p>coming soon</p>
+              </div>
             </div>
           </li>
         </ul>
       </section>
-      <section id={'price'}>
-        <div ref={divRef6}>料金</div>
+      <section id='price'>
+        <div className='outer center' ref={divRef6}>
+          <div className='inner center'>
+            <h1>料金</h1>
+            <p>coming soon</p>
+          </div>
+        </div>
       </section>
-      <section id={'process'}>
-        <div ref={divRef7}>ご契約までの流れ</div>
+      <section id='process'>
+        <div className='outer center' ref={divRef7}>
+          <div className='inner center'>
+            <h1>ご契約までの流れ</h1>
+            <p>coming soon</p>
+          </div>
+        </div>
       </section>
       <style jsx>{`
         ul {
           margin: 0;
         }
-        #title {
+
+        section {
           width: 100%;
           height: 400px;
-          background: #000099;
           position: relative;
+          background: #000099;
         }
-        #title div {
-          width: 80%;
-          height: 80%;
-          background: white;
+
+        .center {
           position: absolute;
           top: 50%;
           left: 50%;
           transform: translate(-50%, -50%);
-          text-align: center;
-          border: 10px solid black;
+        }
+
+        .outer {
+          width: 80%;
+          height: 80%;
+          background: repeating-linear-gradient(-45deg, grey, grey 1%, black 1%, black 2%);
           border-radius: 20px;
           opacity: 0;
         }
-        .bg {
-          width: 50%;
-          height: 100%;
-          position: absolute;
-          background: yellow;
-          zindex: 1;
-          animation: animation1 2s ease-in 0s forwards;
+
+        .inner {
+          width: calc(100% - 20px);
+          height: calc(100% - 20px);
+          margin: auto;
+          background: white;
+          border-radius: 20px;
+          text-align: center;
+          opacity: 1;
         }
+
         #content {
-          width: 100%;
           height: 1600px;
           background: #ffffee;
-          position: relative;
         }
-        #content div {
+
+        #content div.outer {
           width: 40%;
           height: 20%;
-          background: white;
           position: absolute;
-          text-align: center;
-          border: 10px solid black;
-          border-radius: 20px;
-          opacity: 0;
         }
+
         .item1 {
           top: 2.5%;
           left: 5%;
         }
+
         .item2 {
           top: 27.5%;
           left: 55%;
         }
+
         .item3 {
           top: 52.5%;
           left: 5%;
         }
+
         .item4 {
           top: 77.5%;
           left: 55%;
         }
-        #price {
-          width: 100%;
-          height: 400px;
-          background: #000099;
-          position: relative;
-        }
-        #price div {
-          width: 80%;
-          height: 80%;
-          background: white;
-          position: absolute;
-          top: 50%;
-          left: 50%;
-          transform: translate(-50%, -50%);
-          text-align: center;
-          border: 10px solid black;
-          border-radius: 20px;
-          opacity: 0;
-        }
+
         #process {
-          width: 100%;
-          height: 400px;
           background: #ffffee;
-          position: relative;
         }
-        #process div {
-          width: 80%;
-          height: 80%;
-          background: white;
-          position: absolute;
-          top: 50%;
-          left: 50%;
-          transform: translate(-50%, -50%);
-          text-align: center;
-          border: 10px solid black;
-          border-radius: 20px;
-          opacity: 0;
-        }
-        @keyframes animation1 {
-          0% {
-            width: 0;
-          }
-          100% {
-            width: 50%;
-          }
-        }
-        @keyframes animation2 {
+
+        @keyframes fade-in {
           0% {
             opacity: 0;
           }
@@ -367,32 +349,9 @@ const Home: NextPage = () => {
             opacity: 1;
           }
         }
-        .ani2 {
-          animation: animation2 1s ease-in 0s forwards;
-        }
-        @keyframes animation3 {
-          100% {
-            transform: translate(12.5%, -50%);
-          }
-        }
-        .ani3 {
-          animation: animation3 1s ease-in 0s forwards;
-        }
-        @keyframes animation-left {
-          100% {
-            transform: translateX(12.5%);
-          }
-        }
-        .ani-left {
-          animation: animation-left 0.5s ease-in 0s forwards;
-        }
-        @keyframes animation-right {
-          100% {
-            transform: translateX(-112.5%);
-          }
-        }
-        .ani-right {
-          animation: animation-right 0.5s ease-in 0s forwards;
+
+        .fade-in {
+          animation: fade-in 1s ease-in 0s forwards;
         }
       `}</style>
     </Layout>
